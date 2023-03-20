88fc005143445243b9fe934b09c3a039b23c01c6
Maha Abdelnabi <mahaomar@gmail.com>
2023-03-20 02:48:36 -0400

    add answers for prompts

--- app.js
+++ app.js
@@ -54,7 +54,63 @@ const startPrompt = () => {
       },
     ])
     .then((answers) => {
-      // Use user feedback for... whatever!!
+      const { choices } = answers;
+
+      if (choices === "View all departments") {
+        
+      }
+
+      if (choices === "View all roles") {
+        
+      }
+
+      if (choices === "View all employees") {
+        
+      }
+
+      if (choices === "Add a department") {
+        
+      }
+
+      if (choices === "Add a role") {
+        
+      }
+
+      if (choices === "Add an employee") {
+        
+      }
+
+      if (choices === "Update an employee role") {
+        
+      }
+
+      if (choices === "Update an employee manager") {
+       
+      }
+
+      if (choices === "View employees by department") {
+        
+      }
+
+      if (choices === "Delete a department") {
+        
+      }
+
+      if (choices === "Delete a role") {
+        
+      }
+
+      if (choices === "Delete an employee") {
+        
+      }
+
+      if (choices === "View department budgets") {
+        
+      }
+
+      if (choices === "No Action") {
+        
+      }
     })
     .catch((error) => {
       if (error.isTtyError) {
