76efd0ef69304effcb51b0a5254e4c878abbfb66
Maha Abdelnabi <mahaomar@gmail.com>
2023-03-20 02:34:30 -0400

    add startPrompt function

--- app.js
+++ app.js
@@ -17,10 +17,50 @@ const db = mysql.createConnection(
 
 // Establishing Connection to database
 db.connect((err) => {
-    if (err) throw err;
+  if (err) throw err;
 
-    // Start main menu function
+  // Start main menu function
 
-    console.log(" WELCOME TO EMPLOYEE TRACKER ");
-    
+  console.log(" WELCOME TO EMPLOYEE TRACKER ");
+  //run function after connection is established
+  startPrompt();
 });
+
+
+// Prompt User for Choices
+const startPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        name: "choices",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Update an employee manager",
+          "View employees by department",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "View department budgets",
+          "No Action",``
+        ],
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
